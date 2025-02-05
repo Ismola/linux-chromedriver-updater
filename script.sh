9e6d0714d6c6bfefc193285b6f6877699eb3b01c
Ismael Treviño <ismolahiena@gmail.com>
2025-02-05 13:08:23 +0100

    feat(bash): :sparkles: bash file

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-# Paso 1: Obtener la última versión estable de ChromeDriver
+# Paso 1: Instalar dependencias necesarias
+apk add --no-cache curl wget unzip
+
+# Paso 2: Obtener la última versión estable de ChromeDriver
 echo "Obteniendo la última versión estable de ChromeDriver..."
 
-# Descargar el HTML de la página que contiene la versión más reciente y extrae de ese html la url de chromedriver mas reciente
-latest_version=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/#stable | grep -oP 'https://storage.googleapis.com/chrome-for-testing-public/\d+\.\d+\.\d+\.\d+/linux64/chromedriver-linux64.zip' | head -n 1)
+latest_version=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/#stable | grep -o 'https://storage.googleapis.com/chrome-for-testing-public/[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/linux64/chromedriver-linux64.zip' | head -n 1)
 
 # Verificar si se extrajo correctamente la versión
 if [ -z "$latest_version" ]; then
@@ -14,20 +16,17 @@ fi
 
 echo "Última versión: $latest_version"
 
-# Paso 2: Descargar el archivo zip de ChromeDriver
+# Paso 3: Descargar el archivo zip de ChromeDriver
 echo "Descargando ChromeDriver..."
-
 wget "$latest_version" -O chromedriver.zip
 
-# Paso 3: Descomprimir el archivo zip
+# Paso 4: Descomprimir el archivo zip
 echo "Descomprimiendo el archivo..."
-
 unzip -o chromedriver.zip
 
-# Paso 4: Mover el archivo a la carpeta actual
+# Paso 5: Mover el archivo a la carpeta actual
 echo "Moviendo ChromeDriver a la carpeta actual..."
-
-cp chromedriver-linux64/chromedriver .
+mv chromedriver-linux64/chromedriver .
 
 # Limpieza
 rm -rf chromedriver-linux64 chromedriver.zip
